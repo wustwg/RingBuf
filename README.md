a141a698041488e09b792a8ae2a7b280dd497974
wg <940412478@qq.com>
2021-01-31 16:28:15 +0800

    修改文档。

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RingBuf
-A high performance RingBuf!Support read/write,readv/writev.
+A high performance RingBuf base by C++!
+Support read/write,readv/writev.
+
+# 环形缓冲区
+一个基于C++实现的高效的环形缓冲区，代码数量很少，可根据自己的需求扩展接口。
+支持read/write、readv/writev接口。
+
+## 环形缓冲区的适用场景
+1. 需要自己提供read/write接口
+2. 同线程内，支持一边读一边写
+
+## 注意事项
+1. 接口非线程安全。
--- RingBuf.h
+++ RingBuf.h
@@ -11,13 +11,27 @@
 
 class RingBuf {
 public:
+    /**
+     *
+     * @param size 缓冲区大小。
+     */
     explicit RingBuf(ssize_t size);
-    ~RingBuf();
-    ssize_t write(char* buf, ssize_t size);
-    ssize_t read(char* buf, ssize_t size);
 
-    ssize_t writev(struct iovec* src, int size);
-    ssize_t readv(struct iovec* out, int size);
+    ~RingBuf();
+
+    /**
+     *
+     * @param buf
+     * @param size
+     * @return 返回写的数据大小
+     */
+    ssize_t write(char *buf, ssize_t size);
+
+    ssize_t read(char *buf, ssize_t size);
+
+    ssize_t writev(struct iovec *src, int size);
+
+    ssize_t readv(struct iovec *out, int size);
 
     /**
      * 获取缓冲区的可写iovec
@@ -25,32 +39,51 @@ public:
      * @param size 大于0不超过2
      * @return 可写空间的总大小
      */
-    ssize_t getWriteIoVec(struct iovec* out, ssize_t size);
+    ssize_t getWriteIoVec(struct iovec *out, ssize_t size);
+
     /**
      * 拷贝数据之后调用，表示已经拷贝了多少数据到缓冲区中；配合 getWriteIoVec 使用
      * @param ssize 数据大小
      */
     void setWriteSize(ssize_t ssize);
 
-    ssize_t getReadIoVec(struct iovec* out, ssize_t size);
+    ssize_t getReadIoVec(struct iovec *out, ssize_t size);
+
     void setRreadSize(ssize_t ssize);
 
-    static ssize_t copyIoVec(struct iovec* src, int srcSize, struct iovec* dst, int dstSize);
+    static ssize_t copyIoVec(struct iovec *src, int srcSize, struct iovec *dst, int dstSize);
 
     bool empty();
+
+    /**
+     * 获取已写入的数据大小
+     * @return
+     */
     ssize_t dataCount();
+
+    /**
+     * 获取剩余空间大小
+     * @return
+     */
     ssize_t freeCount();
+
 private:
     /**
      * 存储数据的buf指针
      */
-    char * mBuf;
+    char *mBuf;
 
+    /**
+     * 写数据的起始下标
+     */
     ssize_t mWIndex;
+    /**
+     * 读数据的起始下标
+     */
     ssize_t mRIndex;
     const ssize_t mSize;
     /**
-     * 有多少未读数据
+     * 已写入的总数据量
      */
     ssize_t mPending;
 };
